ecc59769f94b39a5b5841fa31ee4ea08a1ad5993
EricTES <erictes96@gmail.com>
2020-01-25 23:52:10 +1100

    Added loop and case for commands

--- robot.rb
+++ robot.rb
@@ -9,10 +9,32 @@ class Robot
 
 
   def calculatedistance(commands)
+    for command in commands
+      direction = command[0]
+      times = command[1].to_i
+
+      case direction
+      when "F"
+        traverse()
+      when "B"
+        traverse()
+      when "L"
+        rotate()
+      when "R"
+        rotate()
+      end
+    end
 
     puts @vertical.abs + @horizontal.abs
   end
 
+  def traverse()
+  
+  end
+
+  def rotate()
+
+  end
 end
 
 com = Array["F1","R5","B2","L5","B3"]
