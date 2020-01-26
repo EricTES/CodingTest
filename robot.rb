dbb10d7d78c84f4320ecf657ee617b77e7264292
EricTES <erictes96@gmail.com>
2020-01-26 17:58:42 +1100

    Add command line prompt for entering commands

--- robot.rb
+++ robot.rb
@@ -1,5 +1,5 @@
 class Robot
-  attr_accessor :axes,:axis, :foward,:backward,:direction
+  attr_accessor :axes,:axis,:foward,:backward,:direction
   def initialize()
     @axes = Array[0,0]
     @axis = 0
@@ -8,31 +8,31 @@ class Robot
     @direction=0
   end
 
-
   def calculatedistance(commands)
-    for command in commands
-      direction = command[0]
-      times = command[1].to_i
+      for command in commands
+        direction = command[0]
+        times = command[1].to_i
 
-      case direction
-      when "F"
-        traverse(@foward * times)
-      when "B"
-        traverse(@backward * times)
-      when "L"
-        rotate("L",times)
-      when "R"
-        rotate("R",times)
+        case direction
+        when "F"
+          traverse(@foward * times)
+        when "B"
+          traverse(@backward * times)
+        when "L"
+          rotate("L",times)
+        when "R"
+          rotate("R",times)
+        end
       end
-    end
-
-    puts @axes[0].abs + @axes[1].abs
+      puts @axes[0].abs + @axes[1].abs
   end
 
+  #Set the axes
   def traverse(times)
     @axes[@axis] += times
   end
 
+  #Set the direction facing
   def rotate(rotation,times)
     if rotation == "R"
       @direction = (@direction + times)%4;
@@ -41,12 +41,13 @@ class Robot
     end
 
     @axis = @direction%2==0? 0:1;
-
     @foward = @direction <= 1? 1:-1;
     @backward = @foward == 1? -1: 1;
-
   end
 end
 
-com = Array["F1","R5","B2","L5","B3"]
-Robot.new.calculatedistance(com)
+
+puts "Enter a line of commands with ',' to seperate them e.g  F1,R5,B2...."
+command = gets.chomp().split(",")
+#command = Array["F1","R5","B2","L5","B3"]
+Robot.new.calculatedistance(command)
